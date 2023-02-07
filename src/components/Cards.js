04c30963a1d64ae04589bcd2037b75a9fec55459
Divine Obeten <divineobeten9@gmail.com>
2023-02-07 15:53:27 +0100

    making cards container responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Cards = () => {
   return (
-    <div>
-      
+    <div className="cards__container">
+      <div className="card">This is a card</div>
+      <div className="card">This is a card</div>
+      <div className="card">This is a card</div>
+      <div className="card">This is a card</div>
     </div>
-  )
+  );
 }
 
 export default Cards
