2525690f75c87c4bc1f93c155863125f2f007948
Divine Obeten <divineobeten9@gmail.com>
2023-02-07 16:30:11 +0100

    creating textarea for card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,10 +3,17 @@ import React from 'react'
 const Cards = () => {
   return (
     <div className="cards__container">
-      <div className="card">This is a card</div>
-      <div className="card">This is a card</div>
-      <div className="card">This is a card</div>
-      <div className="card">This is a card</div>
+      <div className="card">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Let's create a note..."
+          maxLength={200}
+        ></textarea>
+        {/* <input type="text" placeholder="Let's create a note..." max={200} min={1}  /> */}
+      </div>
     </div>
   );
 }
