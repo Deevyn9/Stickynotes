a4740f086ff1fede58f32c10163dea6d476ec2ed
Divine Obeten <divineobeten9@gmail.com>
2023-02-14 10:56:27 +0100

    refactoring handleButtoClick function

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,24 @@
-import {useState, React} from 'react'
+import { useState, React } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Cards = () => {
-    const [notes, setNotes] = useState([]);
-    const [inputValue, setInputValue] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-    const handleInputChange = event => {
-        setInputValue(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    const handleButtonClick = () => {
-        if (inputValue.length >= 3) {
-            setNotes([...notes, inputValue]);
-            setInputValue("");
-        } else {
+  const handleButtonClick = () => {
+    return inputValue.length >= 3 ? (setNotes([...notes, inputValue]), setInputValue("")) : null;
+    //     setInputValue("");
+    // if (inputValue.length >= 3) {
+    //     setNotes([...notes, inputValue]);
+    //     setInputValue("");
+    // } else {
 
-        }
-    };
+    // }
+  };
 
   return (
     <div className="cards__container">
@@ -40,15 +43,18 @@ const Cards = () => {
           </div>
         </div>
       </div>
-    
+
       {notes.map((note, index) => (
         <div key={index} className="card sticky__card">
           <p>{note}</p>
-
+          <div className="date__delete">
+            <p>13/02/2022</p>
+            <img src={DeleteIcon} alt="delete" />
+          </div>
         </div>
       ))}
     </div>
   );
-}
+};
 
-export default Cards
+export default Cards;
